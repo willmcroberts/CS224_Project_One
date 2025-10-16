0e735772c0f1268f53afb9c93c3bb28f47c444d9
willmcroberts <williammcroberts15@gmail.com>
2025-10-16 12:08:01 -0400

    Finished TestInsertionSort.java
    need to do next part of prroject

--- src/TestInsertionSort.java
+++ src/TestInsertionSort.java
@@ -6,11 +6,22 @@ public class TestInsertionSort {
     public static void main(String[] args) {
         // Read data from file
         String inputFileName = "companyBSU.csv";
-        ArrayList<Employee> employeeList = new ArrayList<Employee>();
+        ArrayList<Employee> empList = new ArrayList<Employee>();
         try {
             Scanner sc = new Scanner(new File(inputFileName));
             String header = sc.nextLine();
             System.out.println(header);
+            while (sc.hasNext()) {
+                String empRecord = sc.nextLine();
+                String[] s = empRecord.split(",");
+                Employee empObject = new Employee(Integer.parseInt(s[0]), s[1], s[2], Integer.parseInt(s[3]));
+                empList.add(empObject);
+            }
+            sc.close();
+            for (int i = 0; i < 5; i++) {
+                System.out.println(empList.get(i));
+            }
+
         } catch(Exception e) {
             System.out.println(e);
         }
